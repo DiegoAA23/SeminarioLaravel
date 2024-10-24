8a3859d534cc62d2e13908af8d08adeb6c72d34f
Diego Alvarado <diegoale2304@outlook.com>
2024-10-24 16:21:17 -0600

    Primer Commit

---
+++ app/Http/Controllers/AlumnoController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Alumno;
+
+use Illuminate\Http\Request;
+
+class AlumnoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $alumnos = Alumno::all();
+        //dd($alumnos);
+        return view('alumnos.index', compact('alumnos'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('alumnos.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|min:3|max:255',
+            'lastname' => 'required|string|min:3|max:255',
+            'email' => 'required|string|email|max:255|unique:alumnos',
+            'age' => 'required|integer|between:15,80',
+        ]);
+        Alumno::create([
+            'nombre' => $request->name,
+            'apellido' => $request->apellido,
+            'email' => $request->email,
+            'edad' => $request->edad,
+        ]);
+        return redirect()->route('alumnos.index')
+                         ->with('success', 'Item created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Alumno $alumno)
+    {
+        return view('alumnos.show', compact('alumno'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $alumno = Alumno::findOrFail($id);
+        return view('alumnos.edit', compact('alumno'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $alumno = Alumno::findOrFail($id);
+        // Validar los datos, asegurando que el email sea único, excepto para el alumno actual
+        $request->validate([
+            'nombre' => 'required|string|min:3|max:255',
+            'apellido' => 'required|string|min:3|max:255',
+            'email' => 'required|email|unique:alumnos,email,' . $alumno->id,
+            'edad' => 'required|integer|between:15,80',
+        ]);
+        // Actualizar los datos del alumno
+        $alumno->update($request->all());
+        // Redireccionar con mensaje de éxito
+        return redirect()->route('alumnos.index')->with('success', 'Alumno actualizado correctamente.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Alumno $alumno)
+    {
+        $alumno->delete();
+        return redirect()->route('alumnos.index')->with('success', 'Alumno eliminado correctamente.');
+    }
+}
---
+++ database/migrations/2024_09_26_221412_créate_alumnos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('alumnos', function (Blueprint $table){
+            $table->id();
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('email')->unique();
+            $table->integer('edad');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
